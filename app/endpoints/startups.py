e0c91b84fb0536123fb043f7470a67a79ee2e36b
Sigmanificient <edhyjox@gmail.com>
2025-09-03 10:04:43 +0200

    Allow more modular db queries

--- app/endpoints/startups.py
+++ app/endpoints/startups.py
@@ -6,14 +6,15 @@ from app.db import get_session
 from app.jeb_schema import StartupBase
 
 from ..models import Startup
+from ..helpers.caching_proxy import cached_endpoint, cached_list_endpoint
 
 router = APIRouter()
 
-from ..helpers.caching_proxy import cached_endpoint
 
-
-@cached_endpoint("/startups", db_model=Startup, pydantic_model=StartupBase)
-async def list_startup(db: AsyncSession = Depends(get_session)): ...
+@cached_list_endpoint("/startups", db_model=Startup, pydantic_model=StartupBase)
+async def list_startup(db: AsyncSession = Depends(get_session)):
+    result = await db.execute(select(Startup))
+    return result.scalars().all()
 
 
 @cached_endpoint("/startups/{startup_id}", db_model=Startup, pydantic_model=StartupBase)
--- app/helpers/caching_proxy.py
+++ app/helpers/caching_proxy.py
@@ -1,7 +1,4 @@
-from sqlalchemy import select
-
 from ..config import settings
-from ..models import Startup
 
 import aiohttp
 from fastapi import Depends, APIRouter
@@ -21,36 +18,60 @@ async def fetch_from_api(api_url: str, **kwargs):
         async with session.get(
             url, headers={"X-Group-Authorization": settings.jeb_api_auth}
         ) as response:
+            print("@" * 999)
+            print(response.json())
             return await response.json(), response.status
 
 
-def cached_endpoint(api_url: str, db_model, pydantic_model):
+def cached_endpoint_inner(api_url: str, convert_in, convert_out):
     def decorator(func):
         @wraps(func)
         async def wrapper(db: AsyncSession = Depends(get_session), **kwargs):
-            result = await db.execute(select(db_model))
-            collected = result.scalars().all()
+            collected = await func(**kwargs, db=db)
 
-            if collected is not None:
-                return [
-                    pydantic_model.model_validate(s, from_attributes=True)
-                    for s in collected
-                ]
+            if collected:
+                return convert_out(collected)
 
             res, status = await fetch_from_api(api_url, **kwargs)
 
             if status != HTTPStatus.OK.value:
                 return res
 
-            items = []
-            for item in res:
-                model_instance = pydantic_model(**item)
-                items.append(model_instance)
-                db.add(db_model(**model_instance.model_dump()))
-
+            items = convert_in(db, res)
             await db.commit()
             return items
 
         return router.get("/api" + api_url)(wrapper)
 
     return decorator
+
+
+def cached_list_endpoint(api_url: str, db_model, pydantic_model):
+    def convert_in(db, res):
+        items = []
+        for item in res:
+            model_instance = pydantic_model(**item)
+            items.append(model_instance)
+
+            db.add(db_model(**model_instance.model_dump()))
+
+        return items
+
+    def convert_out(collected):
+        return [
+            pydantic_model.model_validate(s, from_attributes=True) for s in collected
+        ]
+
+    return cached_endpoint_inner(api_url, convert_in, convert_out)
+
+
+def cached_endpoint(api_url: str, db_model, pydantic_model):
+    def convert_in(db, res):
+        model_instance = pydantic_model(**res)
+        db.add(db_model(**model_instance.model_dump()))
+        return model_instance
+
+    def convert_out(collected):
+        return pydantic_model.model_validate(collected, from_attributes=True)
+
+    return cached_endpoint_inner(api_url, convert_in, convert_out)
