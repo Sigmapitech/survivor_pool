9eb3facc4122f128c07d0cd3351a4e92daea7160
Sigmanificient <edhyjox@gmail.com>
2025-09-05 11:02:42 +0200

    Add front-end startup CRUD

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -7,6 +7,7 @@ import Register from "./routes/auth/register";
 import Home from "./routes/home";
 
 import "./index.scss";
+import StartupCRUD from "@/routes/admin/startup-crud";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Root element not found");
@@ -19,6 +20,7 @@ ReactDOM.createRoot(root).render(
       </Route>
       <Route path="/auth/login" element={<Login />} />
       <Route path="/auth/register" element={<Register />} />
+      <Route path="/admin/startup-crud" element={<StartupCRUD />} />
     </Routes>
   </BrowserRouter>
 );
