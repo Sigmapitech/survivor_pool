60173d522c3554db3762d7e0ec677b185e10dc4d
Sigmanificient <edhyjox@gmail.com>
2025-09-11 23:36:52 +0200

    Show a disconnect button after login

--- front/src/layouts/main/navigation-bar/index.tsx
+++ front/src/layouts/main/navigation-bar/index.tsx
@@ -1,8 +1,23 @@
-import { Link } from "react-router";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router";
 
 import "./navbar.scss";
 
 export default function Navbar() {
+  const [token, setToken] = useState<null | string>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    setToken(storedToken);
+  }, []);
+
+  const handleDisconnect = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    navigate("/");
+  };
+
   return (
     <div className="navbar">
       <Link className="navbar-title" to="/">
@@ -13,12 +28,22 @@ export default function Navbar() {
         <Link className="dash-in" to="/dashboard">
           Dashboard
         </Link>
-        <Link className="dash-in" to="/auth/login">
-          Login
-        </Link>
-        <Link className="dash-in" to="/auth/register">
-          Register
-        </Link>
+        {!token && (
+          <>
+            <Link className="dash-in" to="/auth/login">
+              Login
+            </Link>
+            <Link className="dash-in" to="/auth/register">
+              Register
+            </Link>
+          </>
+        )}
+        {token && (
+          // biome-ignore lint: keynote
+          <a className="dash-in" onClick={handleDisconnect}>
+            Disconnect
+          </a>
+        )}
       </div>
     </div>
   );
